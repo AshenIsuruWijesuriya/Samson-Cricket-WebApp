5f719031a7e8bfec7496f610681dce707a1bc69c
Hishen Sandaru Perera <hishenperera@gmail.com>
2025-03-11 11:19:45 +0530

    add navbar fix

--- frontend/src/Common/mainHeader.js
+++ frontend/src/Common/mainHeader.js
@@ -1,17 +1,21 @@
 import React from "react";
 import logo1 from '../assets/images/logo1.png'; 
-import './mainHeader.css';
+import './mainHeader.css'; 
 
 const MainHeader = () => {
     return (
         <header>
-            <div className="header-content">
+            <a href="/" className="header-content">
                 <img src={logo1} alt="Samson Cricket Logo" /> 
                 <h1>SAMSON CRICKET</h1>
-            </div>
+            </a>
             <div className="header-actions">
-                <button className="cart-button">🛒 Cart</button>
-                <button className="login-button">Login</button>
+                <button className="login-button">
+                    Sign In
+                </button>
+                <button className="cart-button">
+                    Cart
+                </button>
             </div>
         </header>
     );
