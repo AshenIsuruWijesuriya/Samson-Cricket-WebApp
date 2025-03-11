917cfaf7fb80050689d6e32c93c3e5b4f2f5d461
HishenPerera <hishenperera@gmail.com>
2025-03-11 19:25:00 +0530

    create consulting page folder

---
+++ frontend/src/Pages/Consulting/Consulting.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainHeader from '../../Common/mainHeader';
+import Navbar from '../../Common/Navbar';
+
+
+const Consulting  = () => {
+    return(
+        <div>
+        <MainHeader/>
+        <Navbar/>
+        <h1>Consulting</h1>
+        </div>
+    )
+};
+
+export default Consulting;
--- frontend/src/Router/Routes.js
+++ frontend/src/Router/Routes.js
@@ -5,6 +5,7 @@ import Shop from '../Pages/Shop/Shop';
 import Coaching from '../Pages/Coaching/Coaching';
 import Services from '../Pages/Services/Service';
 import SignIn from '../Pages/SignIn';
+import Consulting from '../Pages/Consulting/Consulting';
 
 const AppRoutes = () => {
     return (
@@ -12,8 +13,9 @@ const AppRoutes = () => {
             <RouterRoutes>
                 <Route path="/" element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
-                <Route path="/coaching" element={<Coaching />} />
                 <Route path="/services" element={<Services />} />
+                <Route path="/coaching" element={<Coaching />} />
+                <Route path="/consulting" element={<Consulting />} />
 
                 <Route path="/signIn" element={<SignIn />} />
             </RouterRoutes>
