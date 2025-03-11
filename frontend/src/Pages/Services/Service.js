158e894813237784a255b5a062baf324f1d9614e
HishenPerera <hishenperera@gmail.com>
2025-03-11 19:22:22 +0530

    fix services navbar and header

--- frontend/src/Pages/Services/Service.js
+++ frontend/src/Pages/Services/Service.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import MainHeader from '../../Common/mainHeader';
+import Navbar from '../../Common/Navbar';
 
 
 const Services  = () => {
     return(
         <div>
-        
+        <MainHeader/>
+        <Navbar/>
         <h1>Services</h1>
         </div>
     )
