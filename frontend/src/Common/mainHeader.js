485138873761869195a4f9e0b946b83e50a3a54e
HishenPerera <hishenperera@gmail.com>
2025-03-12 19:15:12 +0530

    fix headers

--- frontend/src/Common/mainHeader.js
+++ frontend/src/Common/mainHeader.js
@@ -4,16 +4,13 @@ import './mainHeader.css';
 
 const MainHeader = () => {
     return (
-        <header>
+        <header className="mainHeader">
             <a href="/" className="header-content">
                 <img src={logo1} alt="Samson Cricket Logo" /> 
-                <h1>SAMSON CRICKET</h1>
+                <h1 className="titlename">SAMSON CRICKET</h1>
             </a>
             <div className="header-actions">
                 <a href="/signIn" className="signIn">Sign In</a>
-                <button className="cart-button">
-                    Cart
-                </button>
             </div>
         </header>
     );
