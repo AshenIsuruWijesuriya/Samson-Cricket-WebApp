dff946dff987df6ba7fcace4f4f38a374bea7c45
Hishen Sandaru Perera <hishenperera@gmail.com>
2025-03-17 12:28:10 +0530

    admin dashboard fixes

--- frontend/src/Pages/Admin/AdminDashboard.js
+++ frontend/src/Pages/Admin/AdminDashboard.js
@@ -1,29 +1,48 @@
 import React from 'react';
-import './AdminDashboard.css'; // Import the CSS file
+import './AdminDashboard.css';
 import AdminHeader from './AdminHeader/AdminHeader';
+import { FaUsers, FaBoxes, FaChartLine, FaCommentDots } from 'react-icons/fa';
+
+import usersImage from '../../assets/images/adminbg.jpg'; 
+import inventoryImage from '../../assets/images/adminbg.jpg';
+import reportsImage from '../../assets/images/adminbg.jpg';
+import feedbackImage from '../../assets/images/adminbg.jpg';
 
 const AdminDashboard = () => {
     return (
         <div className='adminbgimg'>
-        <AdminHeader/>
-        <div className='ad-title'>Select a Service</div>
-        <div className="dashboard-container">
-            <a href='/admindashboard/manage-users' className="admin-card-link">
-                <div className="admin-card">
-                    Manage Users
-                </div>
-            </a>
-            <a href='/admindashboard/manage-inventory' className="admin-card-link">
-                <div className="admin-card">
-                    Manage Inventory
-                </div>
-            </a>
-            <a href='/admindashboard/manage-inventory' className="admin-card-link">
-                <div className="admin-card">
-                    Generate Reports
-                </div>
-            </a>
-        </div>
+            <AdminHeader />
+
+            {/* <div className="navigation-path-admindb">
+                 Admin Dashboard
+            </div> */}
+            <div className='ad-title'>Select a Service</div>
+            <div className="dashboard-container">
+                <a href='/admindashboard/manage-users' className="admin-card-link">
+                    <div className="admin-card" style={{ backgroundImage: `url(${usersImage})` }}>
+                        <FaUsers className="admin-card-icon" />
+                        Manage Users
+                    </div>
+                </a>
+                <a href='/admindashboard/manage-inventory' className="admin-card-link">
+                    <div className="admin-card" style={{ backgroundImage: `url(${inventoryImage})` }}>
+                        <FaBoxes className="admin-card-icon" />
+                        Manage Inventory
+                    </div>
+                </a>
+                <a href='/admindashboard/generate-reports' className="admin-card-link">
+                    <div className="admin-card" style={{ backgroundImage: `url(${reportsImage})` }}>
+                        <FaChartLine className="admin-card-icon" />
+                        Generate Reports
+                    </div>
+                </a>
+                <a href='/admindashboard/manage-feedback' className="admin-card-link">
+                    <div className="admin-card" style={{ backgroundImage: `url(${feedbackImage})` }}>
+                        <FaCommentDots className="admin-card-icon" />
+                        Manage Feedbacks
+                    </div>
+                </a>
+            </div>
         </div>
     );
 };
--- frontend/src/Pages/Admin/ManageUsers/ManageUsers.js
+++ frontend/src/Pages/Admin/ManageUsers/ManageUsers.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import AdminHeader from '../AdminHeader/AdminHeader';
+import { FaRegUser, FaUserCog, FaUserTie, FaUserShield, FaMoneyBillAlt } from "react-icons/fa";
+import './ManageUsers.css';
 
 const ManageUsers = () => {
+    return (
+        <div className='manage-users-bg'>
+            <AdminHeader />
 
-    return(
-        <div>
-        <AdminHeader/>
-        <h1>ManageUsers</h1>
+            <div className="navigation-path-admindb">
+                <a href="/admindashboard">Admin Dashboard</a> / Manage Users
+            </div>
+
+            <h2 className='manage-users-heading'>Select user type you want</h2>
+            <div className="manage-users-container">
+                <a href='/' className="manage-user-link">
+                    <div className="manage-user-box manage-customers">
+                        <FaRegUser className="user-box-icon" />
+                        <h2>Manage Customers</h2>
+                    </div>
+                </a>
+                <a href='/' className="manage-user-link">
+                    <div className="manage-user-box manage-administrators">
+                        <FaUserShield className="user-box-icon" />
+                        <h2>Manage Administrators</h2>
+                    </div>
+                </a>
+                <a href='/' className="manage-user-link">
+                    <div className="manage-user-box manage-service-managers">
+                        <FaUserCog className="user-box-icon" />
+                        <h2>Manage Service Managers</h2>
+                    </div>
+                </a>
+                <a href='/' className="manage-user-link">
+                    <div className="manage-user-box manage-coaching-managers">
+                        <FaUserTie className="user-box-icon" />
+                        <h2>Manage Coaching Managers</h2>
+                    </div>
+                </a>
+                <a href='/' className="manage-user-link">
+                    <div className="manage-user-box manage-consultation-managers">
+                        <FaUserTie className="user-box-icon" />
+                        <h2>Manage Consultation Managers</h2>
+                    </div>
+                </a>
+                <a href='/' className="manage-user-link">
+                    <div className="manage-user-box manage-finance-managers">
+                        <FaMoneyBillAlt className="user-box-icon" />
+                        <h2>Manage Finance Managers</h2>
+                    </div>
+                </a>
+            </div>
         </div>
     );
 };
