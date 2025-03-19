562f3ddfcd15821d24fb2fa6c040a6f9dc4ff31f
Nipuna <nipunadbandara@gmail.com>
2025-03-19 12:00:28 +0530

    servicedashboardjs /css fixed

--- frontend/src/Pages/ServiceManager/ServiceManagerDashboard.js
+++ frontend/src/Pages/ServiceManager/ServiceManagerDashboard.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import './ServiceManagerDashboard.css'; // Import the CSS file
 import ServiceManagerHeader from "../ServiceManager/ServicemanagerHeader/ServiceManagerHeader";
+import "./ServiceManagerDashboard.css"; // Import CSS file
 
+// import repairLogo from "../../assets/images/repairlogo.png";
+// import inventoryLogo from "../../assets/images/inventorylogo.jpg";
+// import reportsLogo from "../../assets/images/reportlogo.jpg";
+// import analysisLogo from "../../assets/images/analysis.jpg";
 
 const ServiceManagerDashboard = () => {
     return (
-        <div>
-            <ServiceManagerHeader/>
-            <div className='servicebgimg'></div>
-            <div className='sm-title'>Select a Service</div>
-            <div className="dashboard-container">
-                <a href='/servicemanager/manage-repairs' className="card-link">
-                    <div className="card">
-                        Manage Repairs
+        <div className="sm-dashboard-container">
+            <ServiceManagerHeader />
+
+            {/* Dashboard Title */}
+            <div className="sm-dashboard-title">Service Management</div>
+
+            {/* Service Cards */}
+            <div className="sm-card-container">
+                <a href="/servicemanager/manage-repairs" className="sm-card-link">
+                    <div className="sm-card sm-repair-card">
+                        <span className="sm-card-text">Manage Repairs</span>
                     </div>
                 </a>
-                <a href='/servicemanager/manage-inventory' className="card-link">
-                    <div className="card">
-                        Manage Inventory
+                <a href="/servicemanager/manage-inventory" className="sm-card-link">
+                    <div className="sm-card sm-inventory-card">
+                        <span className="sm-card-text">Repair Inventory</span>
                     </div>
                 </a>
-                <a href='/servicemanager/generate-reports' className="card-link">
-                    <div className="card">
-                        Generate Reports
+                <a href="/servicemanager/generate-reports" className="sm-card-link">
+                    <div className="sm-card sm-reports-card">
+                        <span className="sm-card-text">Generate Reports</span>
+                    </div>
+                </a>
+                <a href="/servicemanager/analysis" className="sm-card-link">
+                    <div className="sm-card sm-analysis-card">
+                        <span className="sm-card-text">Analysis</span>
                     </div>
                 </a>
             </div>
